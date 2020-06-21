c1aa3ca3ea362676a8bc68809c9ae3f5c2a64ea6
🎷🐛 Lee Ball <lee.cat.ball@gmail.com>
2020-06-20 22:16:06 -0700

    Add error-chain boilerplate

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,23 @@
 mod arg_parse;
-// use mimalloc::MiMalloc;
+mod json_cfg;
 
+// ===== Boilerplate for error-chain crate. =====
+#[macro_use]
+extern crate error_chain;
+mod errors { error_chain! { } }
+pub use errors::*;
+quick_main!(run);
+// ===== End error-chain boilerplate. =====
+
+// ===== mimalloc, anecdotally faster, requires cmake and gcc =====
+// use mimalloc::MiMalloc;
 // #[global_allocator]
 // static GLOBAL: MiMalloc = MiMalloc;
+// ===== End mimalloc boilerplate =====
 
-fn main() {
-    let args = arg_parse::get_args();
+/// Main method, before quick_main! composition.
+fn run() {
+    let _args = arg_parse::get_args();
+
     println!("Hello, world!");
 }
